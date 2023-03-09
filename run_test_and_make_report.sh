f3616d6b19fca736dd35c2f2e619807b122649f0
kpochodyla <kpochodyla@protonmail.com>
2023-03-09 20:30:05 +0100

    Configure gh-pages for allure report hosting

--- run_test_and_make_report.sh
+++ run_test_and_make_report.sh
@@ -5,3 +5,4 @@ cp -r allure/allure-report/history allure/allure-results/history
 cp allure/categories.json allure/allure-results/categories.json
 cp allure/environment.properties allure/allure-results/environment.properties
 allure generate allure/allure-results --clean -o allure/allure-report
+cp -r allure/allure-report allure-report
