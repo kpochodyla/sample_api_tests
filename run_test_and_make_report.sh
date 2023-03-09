b46ed415352e5c947e396fdb84d04265afbb6b07
kpochodyla <kpochodyla@protonmail.com>
2023-03-09 22:09:53 +0100

    Run new tests

--- run_test_and_make_report.sh
+++ run_test_and_make_report.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm -r allure/allure-results/
+rm -r allure-report
 pytest --alluredir=allure/allure-results
 cp -r allure/allure-report/history allure/allure-results/history
 cp allure/categories.json allure/allure-results/categories.json
